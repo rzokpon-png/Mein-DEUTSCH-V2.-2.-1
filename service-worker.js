d9f0f9ea05fe22a8aa08bf3e4d8cd8c104c4154a
rzokpon-png <rzokpon@gmail.com>
2026-07-19 08:33:21 +0100

    Update cache version for vocabulary fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mein-deutsch-v2-plat-shell-8";
+const CACHE_NAME = "mein-deutsch-v2-plat-shell-9";
 const APP_SHELL = [
   "./", "./index.html", "./manifest.json", "./style.css",
   "./icon-192.png", "./icon-512.png",
